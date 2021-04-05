c77d8e43641278269ec5679f77132a2668e69e8a
arsenoliinyk <arsen.oliynyk@gmail.com>
2021-04-05 16:54:24 +0300

    Added second method in main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import re
 
 if __name__ == "__main__":
+    #1st method
     count = 0
-    # regex_start = r'\[08\/Mar\/2004\:07\:11\:37'
-    # regex_end = r'\[10\/Mar\/2004\:11\:41\:52'
     file = "D:\\PyCharm programs\\Lab_5\\access_log"
 
     regex = r'\[(0(8\/Mar\/2004)' \
@@ -20,13 +19,32 @@ if __name__ == "__main__":
             r'|(1\:(([0-3][0-9]\:[0-5][0-9]\:[0-5][0-9])|(4((0\:[0-5][0-9])|(1(([0-4][0-9])|(5[0-2])))))))))))' \
             r'\ -0800] ".*" 200' \
 
-
     with open(file, "r") as file:
         for line in file:
             for searched_number in re.finditer(regex, line):
                 count += 1
 
-    print("Successful number of requests = ", count)
+    file.close()
+    print("1) Successful number of requests = ", count)
 
+    #2nd method
+    regex_start = b'08/Mar/2004:07:11:37'
+    regex_end = b'10/Mar/2004:11:41:52'
+    CODE_STATUS = b'200'
+    needed_time = False
+    line_counter = 0
 
+    file = open("D:\\PyCharm programs\\Lab_5\\access_log", 'rb')
+
+    for line in file.readlines():
+        if regex_end in line:
+            needed_time = False
+            break
+        if regex_start in line:
+            needed_time = True
+        if needed_time and CODE_STATUS in line:
+            line_counter += 1
+
+    file.close()
+    print("2) Successful number of requests = ", line_counter)
 
